8b06dba014f356fa4ec9854764ff3d9119263ad3
saigonbitmaster <bitmaster.sg@gmail.com>
2022-03-16 16:48:28 +0700

    init

--- libs/loopback-component-explorer/test/fixtures/dummy-swagger-ui/swagger-ui.js
+++
@@ -1,6 +0,0 @@
-// Copyright IBM Corp. 2014. All Rights Reserved.
-// Node module: creportLib-component-explorer
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-/* custom swagger-ui file */
